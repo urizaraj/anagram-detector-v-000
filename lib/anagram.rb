2a1ed0e2bfda734c116d3e520efba37006c13b60
Alexander Urizar <urizaraj@gmail.com>
2018-02-12 18:26:53 +0000

    Done.

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,20 @@
-# Your code goes here!
+class Anagram
+  attr_accessor :word
+  def initialize(word)
+    self.word = word
+  end
+
+  def match(anagrams)
+    result = anagrams.select do |anagram|
+      word_to_hist(word) == word_to_hist(anagram)
+    end
+  end
+
+  def word_to_hist(word)
+    result = {}
+    word.each_char do |char|
+      result.has_key?(char) ? result[char] += 1 : result[char] = 1
+    end
+    result
+  end
+end
